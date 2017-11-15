d9b47874319decd6e580b5572e270cd51fa3a0b1
ViggoV <vefr86@gmail.com>
2017-11-15 14:16:52 +0100

    forgot to save functions.js

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,5 +1,5 @@
 export function addWrapper (htmlData, params) {
-  let bodyStyle = 'font-family:' + params.font + ' !important; font-size: ' + params.font_size + ' !important; width:100%;'
+  let bodyStyle = 'font-family:' + params.font + (!params.honorFont && '!important')+'; font-size: ' + params.font_size + (!params.honorFontSize && '!important')
   return '<div style="' + bodyStyle + '">' + htmlData + '</div>'
 }
 
